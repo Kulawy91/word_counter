f5e01785bebdcf41b314459ee1f8c7f1a1425af6
Kulawy91 <116515697+Kulawy91@users.noreply.github.com>
2023-09-11 18:35:55 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>  
+using namespace std;
+
+int main()
+{
+	char s[500];
+	cout << "please give the sentence: ";
+	gets_s(s);
+	int word{ 0 };
+	for (int i = 0; s[i] > 0; i++)
+	{
+		if (s[i] == ' ')
+		{
+			word++;
+		}
+	}
+	word = word + 1;
+	cout << "the numbers of words is: " << word;
+
+
+	return 0;
+}
